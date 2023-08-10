5fdea9a13ddb2ad0128624b6b9b98e3495801a4f
dev-hyewon <developer.hyewon@gmail.com>
2023-08-10 20:49:40 +0900

    feat: Add content in skill_stack_article.jsx
    - Added content without styles
    - Organized content into dl lists using loops
    - Categorized skills into Language, Back-end, Database, DevOps, WAS, Cloud, Front-end, Collaboration Tools, Linting Tools
    - Note: Content exceeds skill_stack_article area; to-do for style adjustments

--- src/pages/main/skill_stack_article.jsx
+++ src/pages/main/skill_stack_article.jsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import './styles.css';
 
+const techList = [
+  {
+    title: 'Language',
+    items: ['JAVA', 'Javascript', 'Python', 'C++', 'C', 'C#'],
+  },
+  {
+    title: 'Back-end',
+    items: ['Nodejs', 'Spring'],
+  },
+  {
+    title: 'Database',
+    items: ['Firebase', 'MariaDB', 'MySQL'],
+  },
+  {
+    title: 'DevOps',
+    items: ['Kubernetes', 'Docker', 'jenkins', 'VMWare'],
+  },
+  {
+    title: 'WAS',
+    items: ['NginX', 'Tomcat'],
+  },
+  { title: 'Cloud', items: ['GCP'] },
+  {
+    title: 'Front-end',
+    items: ['React', 'HTML', 'CSS'],
+  },
+  {
+    title: 'Collaboration Tools',
+    items: ['Jira', 'Confluence', 'Gerrit', 'Bitbucket', 'Git'],
+  },
+  {
+    title: 'Linting Tools',
+    items: ['ESLint', 'Prettier'],
+  },
+];
+
 const SkillStackArticle = () => {
   return (
     <article id="main-section-skill" className="section-wrapper">
@@ -12,6 +48,18 @@ const SkillStackArticle = () => {
         </h1>
         <h4>웹을 구성하고 유지 보수할 수 있습니다.</h4>
       </header>
+      <div>
+        <dl>
+          {techList.map((section, i) => (
+            <section key={`skill_section${i}`}>
+              <dt>{section.title}</dt>
+              {section.items.map((item, j) => (
+                <dd key={`skill_section${i}_item${j}`}>{item}</dd>
+              ))}
+            </section>
+          ))}
+        </dl>
+      </div>
     </article>
   );
 };
